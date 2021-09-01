476d9bebaeb83bd8519ed24937d2af6d30bcc8cf
Omar2220 <omar.sh220@gmail.com>
2021-09-01 21:34:33 +0300

    we have built the getters and created the needed sections for our project in addition to pics needed

---
+++ src/handlers/getData.js
@@ -0,0 +1,21 @@
+/*
+//Important:
+if Id>=100 that mean this player don"t have number for his shirt yet
+if Games_played===-1 that mean no data at pares website
+if Minutes_played===-1 that mean no data at pares website
+other fiedls that empty string because no data at pares website
+*/
+const getPlayersName = require("./getJosn");
+
+function getPlayers(request, response) {
+  let body = "";
+  request.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+  request.on("end", () => {
+    const result = getPlayersName();
+    response.writeHead(200, { "content-type": "application/json" });
+    response.end(JSON.stringify(result));
+  });
+}
+module.exports = getPlayers;
---
+++ src/router.js
@@ -0,0 +1,22 @@
+const homeHandler = require("./handlers/home");
+const publicHandler = require("./handlers/public");
+const missingHandler = require("./handlers/missing");
+const getPlayers = require("./handlers/getData");
+const getPlayerCard = require("./handlers/getCard");
+
+function router(request, response) {
+  const url = request.url;
+  if (url === "/") {
+    homeHandler(request, response);
+  } else if (url.includes("public")) {
+    publicHandler(request, response);
+  } else if (url.includes("/card")) {
+    getPlayerCard(request, response);
+  } else if (url.includes("/data")) {
+    getPlayers(request, response);
+  } else {
+    missingHandler(request, response);
+  }
+}
+
+module.exports = router;
