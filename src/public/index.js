1144f237328f1b148c8bb8bc5f9d82f94c916358
Omar2220 <omar.sh220@gmail.com>
2021-09-02 13:29:15 +0300

    moidified the index.js file , made the Previous_clubs obj appear as it should be and fixed it's bugs

--- src/public/index.js
+++ src/public/index.js
@@ -8,6 +8,86 @@ fetch("http://localhost:3000/data")
       options += '<option value="' + Array[i] + '" />';
     }
     document.getElementById("players").innerHTML = options;
+    // .then((Array)=>{
+    //here will be the display to the user (html) in dev with id "dataPreview"
+    const playerToSearch = document.getElementById("playerName").value;
+
+    if (playerToSearch != "") {
+      let PlayerCardHtml = "";
+      if (Array[0]["Id"] >= 100) {
+        PlayerCardHtml += "<div class='shirtNumber'>Not choosen yet</div>";
+      } else {
+        PlayerCardHtml +=
+          "<div class='shirtNumber'>" + Array[0]["Id"] + "</div>";
+      }
+
+      PlayerCardHtml +=
+        "<div class='playerName'>" + Array[0]["Name"] + "</div>";
+      PlayerCardHtml += "<div class='playerCard'>";
+      let styleC =
+        "background-image: url(./public/IMG/" + Array[0]["Id"] + ".png);";
+      PlayerCardHtml +=
+        "<div style='" + styleC + " background-repeat: no-repeat;'>";
+      PlayerCardHtml += "<div>Birthdate: " + Array[0]["Birthdate"] + " </div>";
+      PlayerCardHtml +=
+        "<div>Nationality: " + Array[0]["Nationality"] + " </div>";
+      PlayerCardHtml += "<div>Profile: " + Array[0]["Profile"] + " </div>";
+      PlayerCardHtml +=
+        "<div>Signed_at_PSG: " + Array[0]["Signed_at_PSG"] + " </div>";
+      PlayerCardHtml += "<div>Position: " + Array[0]["Position"] + " </div>";
+      if (Array[0]["Games_played"] > -1) {
+        PlayerCardHtml +=
+          "<div>Games_played: " + Array[0]["Games_played"] + " </div>";
+      } else {
+        PlayerCardHtml += "<div>Games_played: No Data To Preview </div>";
+      }
+      if (Array[0]["Minutes_played"] > -1) {
+        PlayerCardHtml +=
+          "<div>Minutes_played: " + Array[0]["Minutes_played"] + " </div>";
+      } else {
+        PlayerCardHtml += "<div>Minutes_played: No Data To Preview </div>";
+      }
+      //previous clubs
+      const arr = Array[0]["PREVIOUS_CLUBS"];
+
+      if (arr.length > 0) {
+        //const newArr = arr.split(",");
+        PlayerCardHtml += "<div>PREVIOUS_CLUBS:</div>";
+        //for(let x=0;x<newArr.length;x++){
+        // PlayerCardHtml += "<div>" + newArr[x]+" </div>"
+
+        //}
+      } else {
+        PlayerCardHtml += "<div>PREVIOUS_CLUBS: No Data To Preview.</div>";
+      }
+      if (Array[0]["Website"] != "") {
+        PlayerCardHtml +=
+          "<div>Website: <a href='" +
+          Array[0]["Website"] +
+          "' target='_blank'>" +
+          Array[0]["Name"] +
+          "</a></a></div>";
+      } else {
+        PlayerCardHtml += "<div>Website: No data</div>";
+      }
+      if (Array[0]["Facebook"] != "") {
+        PlayerCardHtml +=
+          "<div>Facebook: <a href='" +
+          Array[0]["Facebook"] +
+          "' target='_blank'>" +
+          Array[0]["Name"] +
+          "</a></a></div>";
+      } else {
+        PlayerCardHtml += "<div>Facebook: No data</div>";
+      }
+
+      PlayerCardHtml += "</div>";
+      PlayerCardHtml += "</div>";
+      //console.log(PlayerCardHtml);
+      document.getElementById("dataPreview").innerHTML = PlayerCardHtml;
+    } else {
+      document.getElementById("dataPreview").innerHTML = "Paris";
+    }
   })
   .catch((err) => {
     console.log(err);
@@ -19,89 +99,95 @@ document.querySelector("form").addEventListener("submit", (event) => {
     .then((response) => {
       return response.json();
     })
-    .then((Array) => {
+    .then((arr2) => {
       //here will be the display to the user (html) in dev with id "dataPreview"
       if (playerToSearch != "") {
         let PlayerCardHtml = "<div class='shrtNum_plyrName'>";
-        if (Array[0]["Id"] >= 100) {
+        if (arr2[0]["Id"] >= 100) {
           PlayerCardHtml += "<div class='shirtNumber'>Not choosen yet</div>";
         } else {
           PlayerCardHtml +=
-            "<div class='shirtNumber'>" + Array[0]["Id"] + "</div>";
+            "<div class='shirtNumber'>" + arr2[0]["Id"] + "</div>";
         }
 
+        console.log(1);
+
         PlayerCardHtml +=
-          "<div class='playerName'>" + Array[0]["Name"] + "</div>";
+          "<div class='playerName'>" + arr2[0]["Name"] + "</div>";
         PlayerCardHtml += "</div>";
         PlayerCardHtml += "<div class='playerCard'>";
-        //PlayerCardHtml += "<div><img src='./public/IMG/"+Array[0]['Id']+".png'></div>"
+        //PlayerCardHtml += "<div><img src='./public/IMG/"+arr2[0]['Id']+".png'></div>"
         let styleC =
-          "background-image: url(./public/IMG/" + Array[0]["Id"] + ".png);";
+          "background-image: url(./public/IMG/" + arr2[0]["Id"] + ".png);";
         PlayerCardHtml +=
           "<div class='Img_other' style='" +
           styleC +
           " background-repeat: no-repeat;'>";
         PlayerCardHtml +=
-          "<div class='other'>Birthdate : " + Array[0]["Birthdate"] + " </div>";
+          "<div class='other'>Birthdate : " + arr2[0]["Birthdate"] + " </div>";
         PlayerCardHtml +=
           "<div class='other'>Nationality : " +
-          Array[0]["Nationality"] +
+          arr2[0]["Nationality"] +
           " </div>";
         PlayerCardHtml +=
-          "<div class='other'>Profile : " + Array[0]["Profile"] + " </div>";
+          "<div class='other'>Profile : " + arr2[0]["Profile"] + " </div>";
         PlayerCardHtml +=
           "<div class='other'>Signed_at_PSG : " +
-          Array[0]["Signed_at_PSG"] +
+          arr2[0]["Signed_at_PSG"] +
           " </div>";
         PlayerCardHtml +=
-          "<div class='other'>Position : " + Array[0]["Position"] + " </div>";
-        if (Array[0]["Games_played"] > -1) {
+          "<div class='other'>Position : " + arr2[0]["Position"] + " </div>";
+        if (arr2[0]["Games_played"] > -1) {
           PlayerCardHtml +=
             "<div class='other'>Games_played : " +
-            Array[0]["Games_played"] +
+            arr2[0]["Games_played"] +
             " </div>";
         } else {
           PlayerCardHtml +=
             "<div class='other'>Games_played : No Data To Preview </div>";
         }
-        if (Array[0]["Minutes_played"] > -1) {
+        if (arr2[0]["Minutes_played"] > -1) {
           PlayerCardHtml +=
             "<div class='other'>Minutes_played : " +
-            Array[0]["Minutes_played"] +
+            arr2[0]["Minutes_played"] +
             " </div>";
         } else {
           PlayerCardHtml +=
             "<div class='other'>Minutes_played : No Data To Preview </div>";
         }
         //previous clubs
-        PlayerCardHtml +=
-          "<div class='other'>PREVIOUS_CLUBS : " +
-          Array[0]["PREVIOUS_CLUBS"][0] +
-          " </div>";
-        let arr = Array[0]["PREVIOUS_CLUBS"][0];
-        console.log(arr);
-        for (let club in arr) {
-          console.log(club, arr[club]);
-        }
-        // arr.forEach(element => {
-        //   console.log(element);
-        // });
-        if (Array[0]["Website"] != "") {
+        PlayerCardHtml += "<div class='other'>PREVIOUS_CLUBS : " + " </div>";
+
+        console.log("AZIZ", arr2[0]["PREVIOUS_CLUBS"][0]["Santos FC"]);
+        let arr = arr2[0]["PREVIOUS_CLUBS"];
+
+        PlayerCardHtml += "<div class='other'>";
+
+        // Object.keys(obj) ['santos Fc']
+
+        arr.forEach((obj) => {
+          const key = Object.keys(obj)[0];
+          PlayerCardHtml += `<span>${key} ${obj[key]} </span>`;
+        });
+
+        PlayerCardHtml += "</div>";
+
+        if (arr2[0]["Website"] != "") {
           PlayerCardHtml +=
             "<div class='other'>Website : <a href='" +
-            Array[0]["Website"] +
+            arr2[0]["Website"] +
             "' target='_blank'>" +
-            Array[0]["Name"] +
+            arr2[0]["Name"] +
             "</a></a></div>";
         } else {
           PlayerCardHtml += "<div class='other'>Website : No data</div>";
         }
-        if (Array[0]["Facebook"] != "") {
+        if (arr2[0]["Facebook"] != "") {
           PlayerCardHtml +=
             "<div class='other'>Facebook : <a href='" +
-            Array[0]["Facebook"] +
+            arr2[0]["Facebook"] +
             "' target='_blank'>" +
-            Array[0]["Name"] +
+            arr2[0]["Name"] +
             "</a></a></div>";
         } else {
           PlayerCardHtml += "<div class='other'>Facebook: No data</div>";
